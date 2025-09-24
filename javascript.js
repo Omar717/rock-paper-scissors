4f1566c9eaee3df9862361b91d9f659d15661f6a
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2025-09-24 17:20:47 +0100

    Add function which plays one round
    Written function which contains logic to play one round of rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -15,3 +15,28 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! paper beats rock");
+        return ++computerScore;    
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! rock beats scissors");
+        return ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! paper beats rock");
+        return ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! scissors beats paper");
+        return ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! rock beats scissors");
+        return ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! scissors beats paper");
+        return ++humanScore;
+    } else {
+        console.log("It's a draw!")
+    }        
+}
