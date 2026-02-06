ad0e9d78175bbe2235d7111e83b131366a3e4909
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-02-06 13:51:07 +0000

    Make improvements
    Add static features to html and dynamic features to javascript

--- javascript.js
+++ javascript.js
@@ -8,68 +8,70 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let response = prompt("Choose rock, paper or scissors");
-    return response;
-}
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
     const result = document.querySelector(".result");
+    const humanCount = document.querySelector(".humanScore");
+    const computerCount = document.querySelector(".computerScore");
+    const winner = document.querySelector(".winner");
 
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
         counter = humanScore + computerScore;
+
         if (humanChoice === "rock" && computerChoice === "paper") {
             result.textContent = "You lose! paper beats rock";
-            return computerScore = ++computerScore;    
+            computerScore = ++computerScore; 
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);  
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
             result.textContent = "You win! rock beats scissors";
-            return humanScore = ++humanScore;
+            humanScore = ++humanScore;
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "paper" && computerChoice === "rock") {
             result.textContent = "You win! paper beats rock";
-            return humanScore = ++humanScore;
+            humanScore = ++humanScore;
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "paper" && computerChoice === "scissors") {
             result.textContent = "You lose! scissors beats paper";
-            return computerScore = ++computerScore;
+            computerScore = ++computerScore;
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "scissors" && computerChoice === "rock") {
             result.textContent = "You lose! rock beats scissors";
-            return computerScore = ++computerScore;
+            computerScore = ++computerScore;
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
             result.textContent = "You win! scissors beats paper";
-            return humanScore = ++humanScore;
+            humanScore = ++humanScore;
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else {
             result.textContent = "It's a draw!";
+            humanCount.textContent = "Your Score: " + String(humanScore);
+            computerCount.textContent = "Computer's Score: " + String(computerScore);
         }
+
+        if (humanScore == 5) {
+            return winner.textContent = "Congratulations! You win";
+        } else if (computerScore == 5) {
+            return winner.textContent = "You lost. Too bad";
+        }
+
     }
 
-    const rockBtn = document.createElement("button");
-    const paperBtn = document.createElement("button");
-    const scissorsBtn = document.createElement("button");
-
-    const container = document.querySelector(".container");
-
-    rockBtn.textContent = "Rock";
-    paperBtn.textContent = "Paper";
-    scissorsBtn.textContent = "Scissors";
-
-    container.appendChild(rockBtn);
-    container.appendChild(paperBtn);
-    container.appendChild(scissorsBtn);
+    const rockBtn = document.querySelector(".rockBtn");
+    const paperBtn = document.querySelector(".paperBtn");
+    const scissorsBtn = document.querySelector(".scissorsBtn");
 
     rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
     paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
     scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
-
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You win");
-    } else if (computerScore > humanScore) {
-        console.log("You lost. Too bad");
-    } else {
-        console.log("It's a draw");
-    }
 }
 
 playGame()
