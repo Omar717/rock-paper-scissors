d3c89efe537de5426094c18799a6a09376005000
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-02-06 12:29:53 +0000

    Fix buttons
    Fix the event listeners once clicked

--- javascript.js
+++ javascript.js
@@ -17,29 +17,31 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    const result = document.querySelector(".result");
+
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
         counter = humanScore + computerScore;
         if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("You lose! paper beats rock");
+            result.textContent = "You lose! paper beats rock";
             return computerScore = ++computerScore;    
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! rock beats scissors");
+            result.textContent = "You win! rock beats scissors";
             return humanScore = ++humanScore;
         } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! paper beats rock");
+            result.textContent = "You win! paper beats rock";
             return humanScore = ++humanScore;
         } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("You lose! scissors beats paper");
+            result.textContent = "You lose! scissors beats paper";
             return computerScore = ++computerScore;
         } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! rock beats scissors");
+            result.textContent = "You lose! rock beats scissors";
             return computerScore = ++computerScore;
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! scissors beats paper");
+            result.textContent = "You win! scissors beats paper";
             return humanScore = ++humanScore;
         } else {
-            console.log("It's a draw!");
+            result.textContent = "It's a draw!";
         }
     }
 
@@ -57,9 +59,9 @@ function playGame() {
     container.appendChild(paperBtn);
     container.appendChild(scissorsBtn);
 
-    rockBtn.addEventListener("click", playRound("rock", getComputerChoice));
-    paperBtn.addEventListener("click", playRound("paper", getComputerChoice));
-    scissorsBtn.addEventListener("click", playRound("scissors", getComputerChoice));
+    rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
+    paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
+    scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
 
     if (humanScore > computerScore) {
         console.log("Congratulations! You win");
