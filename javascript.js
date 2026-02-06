fc8eb262e74c11718cc5c7a7759f7ac8959280ad
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-02-06 21:20:47 +0000

    Add restart
    Added an automatic restart function after someone wins

--- javascript.js
+++ javascript.js
@@ -33,32 +33,32 @@ function playGame() {
         counter = humanScore + computerScore;
 
         if (humanChoice === "rock" && computerChoice === "paper") {
-            result.textContent = "You lose! paper beats rock";
+            result.textContent = "paper beats rock";
             computerScore = ++computerScore; 
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);  
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            result.textContent = "You win! rock beats scissors";
+            result.textContent = "rock beats scissors";
             humanScore = ++humanScore;
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "paper" && computerChoice === "rock") {
-            result.textContent = "You win! paper beats rock";
+            result.textContent = "paper beats rock";
             humanScore = ++humanScore;
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            result.textContent = "You lose! scissors beats paper";
-            computerScore = ++computerScore;
+            result.textContent = "scissors beats paper";
+            computerScore = ++computerScore; 
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            result.textContent = "You lose! rock beats scissors";
-            computerScore = ++computerScore;
+            result.textContent = "rock beats scissors";
+            computerScore = ++computerScore; 
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            result.textContent = "You win! scissors beats paper";
+            result.textContent = "scissors beats paper";
             humanScore = ++humanScore;
             humanCount.textContent = "Your Score: " + String(humanScore);
             computerCount.textContent = "Computer's Score: " + String(computerScore);
@@ -69,9 +69,24 @@ function playGame() {
         }
 
         if (humanScore == 5 && (humanScore > computerScore)) {
-            return winner.textContent = "Congratulations! You win";
+            winner.textContent = "Congratulations! You win";
         } else if (computerScore == 5 && (computerScore > humanScore)) {
-            return winner.textContent = "You lost. Too bad";
+            winner.textContent = "You lost. Too bad";
+        }
+
+        function dialogBox() {
+            alert("Game Over")
+            location.reload();
+        }
+        
+        if (humanScore == 5 && (humanScore > computerScore)) {
+            rockBtn.addEventListener("click", dialogBox);
+            paperBtn.addEventListener("click", dialogBox);
+            scissorsBtn.addEventListener("click", dialogBox);
+        } else if (computerScore == 5 && (computerScore > humanScore)) {
+            rockBtn.addEventListener("click", dialogBox);
+            paperBtn.addEventListener("click", dialogBox);
+            scissorsBtn.addEventListener("click", dialogBox);
         }
     }
 }
